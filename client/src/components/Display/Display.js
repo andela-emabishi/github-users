0afc067ea1a5b6b826e97991a9aac3a0f8d68b37
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-09-22 17:39:48 +0300

    [Feat] Apply style to Display component

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import './card.css';
 
 export default class Display extends React.Component {
-  handleRepoClick = (user) => event => {
-    event.preventDefault();
-    const login = event.target.value;
-    this.props.handleRepoClick(user);
-  }
-  render() {
-    return (
-      <ul>
-        {
-          this.props.users.map((user, index) => (
-            <li key={user.id}>
-              <img src={user.avatar_url} alt=""/>
-              <h4>{user.login}</h4>
-              <a>Repositories</a>
-              <button onClick={this.handleRepoClick(user)}>CLICK HERE</button>
-            </li>
-          ))
-        }
-      </ul>
-    )
-  }
-}
+
+                 handleRepoClick = user => event => {
+                   event.preventDefault();
+                   const login = event.target.value;
+                   this.props.handleRepoClick(user);
+                 };
+                 render() {
+                   return <div className="ui link cards">
+                       {this.props.users.map((user, index) => (
+                         <div className="ui centered raised orange card" key={user.id}>
+                           <a className="image">
+                             <img src={user.avatar_url} alt="" />
+                           </a>
+                           <div className="content">
+                             <a className="header">
+                               {user.login}
+                             </a>
+                             <div className="meta">
+                               <a>{user.email}</a>
+                               <br />
+                               <a
+                                 href={`https://github.com/${user.login}`}
+                               >
+                                 Account
+                               </a>
+                               <a
+                                 href={`https://github.com/${user.login}/?tab=repositories`}
+                               >
+                                 Repositories
+                               </a>
+                               <a
+                                 href={`https://github.com/${user.login}?tab=followers`}
+                               >
+                                 Followers
+                               </a>
+                               <br />
+                               <a>{user.location}</a>
+                             </div>
+                           </div>
+                         </div>
+                       ))}
+                     </div>;
+                 }
+               }
