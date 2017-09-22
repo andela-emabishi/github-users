128bc04d1f49f51ddd0cfb58443494136492c4c3
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-09-22 18:16:47 +0300

    [Chore] Style UserDetails component

--- client/src/components/UserDetails/UserDetails.js
+++ client/src/components/UserDetails/UserDetails.js
@@ -7,27 +7,27 @@ export default class UserDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numberOfFollowers: ''
+      user: {},
     }
   }
 
   componentDidMount = () => {
-    this.fetchFollowers()
+    this.fetchUser()
   }
 
-  fetchFollowers() {
-    axios.get(`https://api.github.com/users/${this.props.login}/followers`)
+  fetchUser() {
+    axios.get(`https://api.github.com/users/${this.props.user.login}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`)
     .then((response) => {
-    const followers = response.data
-    const numberOfFollowers = followers.length
-    this.setState({ numberOfFollowers })}
-  )
+      const user = response.data;
+      console.log('user', user)
+      this.setState({
+        user
+      })
+    })
   }
 
   render() {
-    console.log('followers', this.state.numberOfFollowers)
-    return (
-      <div className="ui centered raised orange card">
+    return <div className="ui centered raised orange card">
         <a className="image">
           <img src={this.props.user.avatar_url} alt="" />
         </a>
@@ -35,18 +35,22 @@ export default class UserDetails extends React.Component {
           <a className="header">{this.props.user.login}</a>
           <div className="meta">
             <br />
-            <a href={`https://github.com/${this.props.user.login}`}>Account</a>
-            <a href={`https://github.com/${this.props.user.login}/?tab=repositories`}>
-              Repositories
-            </a>
-            <a href={`https://github.com/${this.props.user.login}?tab=followers`}>
-              Followers
-            </a>
-            <a>{this.state.numberOfFollowers}</a>
+            <span>Blog</span>
+            <br />
+            <a>{this.state.user.blog}</a>
+            <br />
+            <span>Location</span>
+            <br />
+            <span>{this.state.user.location}</span>
+            <br />
+            <span>Followers</span>
+            <span>{this.state.user.followers}</span>
+            <br />
+            <span>Following</span>
+            <span>{this.state.user.following}</span>
             <br />
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
